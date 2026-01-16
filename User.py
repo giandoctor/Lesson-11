9ba2f8c52de48edb7ef6dc8704347758bfcf1804
giandoctor <gian.doctor@hotmail.com>
2026-01-16 13:19:27 +1100

    user

---
+++ User.py
@@ -0,0 +1,13 @@
+n=int(input("Enter a number:"))
+i=9
+while i >= n:
+    print("1 digit number")
+h=99
+if h >= n:
+    print("2 digit number")
+f=999
+if f >= n:
+    print("3 digit number")
+else:
+    print("So on")
+    
